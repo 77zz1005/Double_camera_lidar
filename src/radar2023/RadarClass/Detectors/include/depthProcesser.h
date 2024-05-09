3dabda638106b21484a04b7c910832d2c7aa24d2
77zz1005 <1316946585@qq.com>
2024-05-09 10:31:44 +0800

    尝试解决头文件编译问题

--- src/radar2023/RadarClass/Detectors/include/depthProcesser.h
+++ src/radar2023/RadarClass/Detectors/include/depthProcesser.h
@@ -15,20 +15,25 @@ public:
 
 private:
     bool _initflag = false;
-    vector<queue<Matrix<int, 2, MaxPointsNum>>> processQueue(2);
-
-    vector<Matrix<float, 3, 3>> K_0(2);
-    vector<Matrix<float, 1, 5>> C_0(2);
-    vector<Matrix<float, 4, 4>> E_0(2);
-    vector<vector<vector<float>>> depth(2);
+    vector<queue<Matrix<int, 2, MaxPointsNum>>> processQueue = vector<queue<Matrix<int, 2, MaxPointsNum>>>(2);
+    // C++
+    /*
+    在类定义中，不能直接使用括号 () 初始化成员变量。应该使用等号 = 或花括号 {} 进行
+    参考：https://cplusplus.com/forum/beginner/240393/
+    参考：https://stackoverflow.com/questions/11490988/c-compile-time-error-expected-identifier-before-numeric-constant
+    */
+    vector<Matrix<float, 3, 3>> K_0 = vector<Matrix<float, 3, 3>>(2);
+    vector<Matrix<float, 1, 5>> C_0 = vector<Matrix<float, 1, 5>>(2);
+    vector<Matrix<float, 4, 4>> E_0 = vector<Matrix<float, 4, 4>>(2);
+    vector<vector<vector<float>>> depth = vector<vector<vector<float>>>(2);
 
 public:
     DepthQueue();
     DepthQueue(vector<Matrix<float, 3, 3>> &K_0, vector<Matrix<float, 1, 5>> &C_0, vector<Matrix<float, 4, 4>> &E_0);
     ~DepthQueue();
 
-    //好像没用
-    // int getProcessQueueSize() { return processQueue[camera_index].size(); }
+    // 好像没用
+    //  int getProcessQueueSize() { return processQueue[camera_index].size(); }
 
     vector<vector<float>> pushback(int camera_index, pcl::PointCloud<pcl::PointXYZ> &pc);
 };
